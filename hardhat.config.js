a8be1f384e533d07f7a0f0f80328e8dc45d65d3b
jay-trends <143098921+jay-trends@users.noreply.github.com>
2023-08-31 12:19:33 +0800

    Adding gas report

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,11 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require('@nomiclabs/hardhat-truffle5');
 require('solidity-coverage')
+require("hardhat-gas-reporter");
 
 module.exports = {
     solidity: "0.8.17",
+    gasReporter: {
+        enabled: true
+    }
 };
